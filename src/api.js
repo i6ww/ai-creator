215199d7432b00bc1c4f1f264588a112ef3a3e39
秃头邢 <admin@371181668.xyz>
2026-04-23 05:11:55 +0800

    恢复Worker代理地址，后端服务器无CORS配置

--- src/api.js
+++ src/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { getNewApiUserId, getAccessToken } from './utils/auth';
 
 const isDev = import.meta.env.DEV;
-const API_BASE_URL = isDev ? '' : 'https://www.371181668.xyz';
+const API_BASE_URL = isDev ? '' : 'https://ai-creator.i6ww.workers.dev';
 
 const api = axios.create({
   baseURL: API_BASE_URL,
